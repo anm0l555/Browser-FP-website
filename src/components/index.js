2c596a8404336c09d9cab41fb8e51c9ba093763d
anm0l555 <atiwari4_be19@thapar.edu>
2022-05-10 02:28:22 +0530

    Logos in main page added

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,12 @@
 import { React, useState, useEffect } from "react";
 import Image1 from "./images/hehe.png";
+import CanvasFP from "./images/Fingerprints/cf.png";
 import { Link } from "react-router-dom";
 import Animation from "./animation/animate";
 import "./index.css";
 import Navbar from "./navbar/Navbar";
 import Loader from "./loader/loader";
+
 function Index() {
   const [loading, setLoading] = useState(true);
   useEffect(() => {
@@ -30,7 +32,7 @@ function Index() {
               <br />
               <br />
               <Link to="/fingerprint">
-                <a href="#!"  className="btn left" id="gmf-index">
+                <a href="#!" className="btn left" id="gmf-index">
                   GET MY FINGERPRINT
                 </a>
               </Link>
@@ -38,6 +40,50 @@ function Index() {
             <div className="col s12 m12 l6 secondRowIndex left">
               <Animation />
               {/* <img src={Image1} className="center img-secrow" alt="fingerprint" /> */}
+            </div>
+          </div>
+          <div className="row SecondDivRowIndex">
+            <div className="col s12 m4 l2">
+              <img
+                src={CanvasFP}
+                className="responsive-img"
+                alt="canvas Fingerprint"
+              />
+            </div>
+            <div className="col s12 m4 l2">
+              <img
+                src={CanvasFP}
+                className="responsive-img"
+                alt="canvas Fingerprint"
+              />
+            </div>
+            <div className="col s12 m4 l2">
+              <img
+                src={CanvasFP}
+                className="responsive-img"
+                alt="canvas Fingerprint"
+              />
+            </div>
+            <div className="col s12 m4 l2">
+              <img
+                src={CanvasFP}
+                className="responsive-img"
+                alt="canvas Fingerprint"
+              />
+            </div>
+            <div className="col s12 m4 l2">
+              <img
+                src={CanvasFP}
+                className="responsive-img"
+                alt="canvas Fingerprint"
+              />
+            </div>
+            <div className="col s12 m4 l2">
+              <img
+                src={CanvasFP}
+                className="responsive-img"
+                alt="canvas Fingerprint"
+              />
             </div>
           </div>
         </>
